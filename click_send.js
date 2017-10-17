8945ef8b0a2c57b4805c2a37cb4f32d152624045
Smriti Narayan <smriti.2610@gmail.com>
2017-10-17 13:52:05 +0530

    Add via upload- click capture

---
+++ click_send.js
@@ -0,0 +1,27 @@
+<script>
+if (document.addEventListener) {
+    document.getElementsByTagName("body")[0].addEventListener("click", addclickpt);
+} else if (document.attachEvent) {
+    document.getElementsByTagName("body")[0].attachEvent("onclick", addclickpt);
+}
+function addclickpt()
+{
+	var x = event.clientX;
+    var y = event.clientY;
+    var d = new Date();
+	var t=d.getHours()+":"+d.getMinutes()+":"+d.getSeconds();
+    var coords = JSON.stringify({"x":x,"y":y,"time":t});
+
+    //send the coords to the server for database updates
+
+    var xhr = new XMLHttpRequest();
+  	xhr.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+    console.log("sending");
+    }
+  	};
+  	xhr.open("POST", "/store", true);
+  	xhr.setRequestHeader("Content-type", "application/json");
+  	xhr.send(coords);
+}
+</script>
