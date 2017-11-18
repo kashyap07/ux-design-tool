8c7df54a6ffcd9652a1883f1c9cc1631833c6af5
Suhas Kashyap <kashyapsuhas07@gmail.com>
2017-11-18 19:54:31 +0530

    Added question boxes, fixed links and names, added html pages for all features, added a payment page
    Payment page from https://codepen.io/supah/pen/OMdPpW

--- static/js/discussion-box.js
+++ static/js/discussion-box.js
@@ -5,11 +5,13 @@ function expand(obj) {
 		obj.className = 'btn-expand-clicked btn-expand';
 		setTimeout(function () {
 				$('.discussion-box').removeClass('hidden');
+				$('.discussion-qs').removeClass('hidden');
 		}, 300);
 	}
 }
 
 function closeBox() {
 	$('.discussion-box').addClass('hidden');
+	$('.discussion-qs').addClass('hidden');
 	$('.btn-expand-clicked.btn-expand').removeClass('btn-expand-clicked');
 }
